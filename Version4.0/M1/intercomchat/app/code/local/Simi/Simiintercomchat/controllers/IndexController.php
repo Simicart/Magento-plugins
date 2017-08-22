e386803f9d478a7d2045b7483807d64456fc4e66
Peter <peter@simicart.com>
2017-08-22 15:36:50 +0700

    intercom chat

---
+++ Version4.0/M1/intercomchat/app/code/local/Simi/Simiintercomchat/controllers/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ * 
+ * @category    
+ * @package     Simiintercomchat
+ * @copyright   Copyright (c) 2012
+ * @license     
+ */
+
+/**
+ * Simiintercomchat Controller
+ * 
+ * @category    
+ * @package     Simiintercomchat
+ * @author      Developer
+ */
+class Simi_Simiintercomchat_IndexController extends Mage_Core_Controller_Front_Action {
+
+    public function checkInstallAction() {
+        echo "1";
+
+        exit();
+    }
+}
