af915f06b38dec088f789cd6179b78b0ecd878bd
Cody Nguyen <cody@simicart.com>
2018-04-06 14:19:42 +0700

    change controller name 2

---
+++ Version4.0/M2/rewardpoints/app/code/Simi/Simirewardpoints/Controller/Adminhtml/Spendingrates/Validate.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Simi\Simirewardpoints\Controller\Adminhtml\Spendingrates;
+
+class Validate extends \Magento\Customer\Controller\Adminhtml\Index
+{
+
+    /**
+     * AJAX customer validation action
+     *
+     * @return \Magento\Framework\Controller\Result\Json
+     */
+    public function execute()
+    {
+        $response = new \Magento\Framework\DataObject();
+        $response->setError(0);
+        $resultJson = $this->resultJsonFactory->create();
+        $resultJson->setData($response);
+        return $resultJson;
+    }
+
+    /**
+     * Check the permission to Manage Customers
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Simi_Simirewardpoints::Spending_Rates');
+    }
+}
