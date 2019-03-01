1ae4fc2a8c3307159c292d37f792436a96f25773
Cody Nguyen <cody@simicart.com>
2019-03-01 14:04:35 +0700

    Move placement action

--- Version4.0/M2/custompayment/app/code/Simi/Simicustompayment/Controller/Api/Placement.php
+++ Version4.0/M2/custompayment/app/code/Simi/Simicustompayment/Controller/Api/Placement.php
@@ -5,7 +5,7 @@
  * Copyright © 2016 Simicommerce. All rights reserved.
  */
 
-namespace Simi\Simicustompayment\Controller\Index;
+namespace Simi\Simicustompayment\Controller\Api;
 
 class Placement extends \Magento\Framework\App\Action\Action
 {
@@ -14,17 +14,6 @@ class Placement extends \Magento\Framework\App\Action\Action
     {
         $simiObjectManager = $this->_objectManager;
         $session = $simiObjectManager->get('Magento\Customer\Model\Session');
-        $storeManager  = $simiObjectManager->create('\Magento\Store\Model\StoreManagerInterface');
-        $websiteId = $storeManager->getStore()->getWebsiteId();
-        $customer  = $simiObjectManager->get('Magento\Customer\Model\Customer')
-            ->setWebsiteId($websiteId);
-
-        try {
-            $customer->loadByEmail(base64_decode($this->getRequest()->getParam('Email')));
-            $loginSession = $session->setCustomerAsLoggedIn($customer);
-        }catch(Exception $e){
-
-        }
         $checkoutSession = $simiObjectManager->create('Magento\Checkout\Model\Session');
         $checkoutSession->setOrderid(base64_decode($this->getRequest()->getParam('OrderID')));
         $checkoutSession->setMerchantid(base64_decode(($this->getRequest()->getParam('MerchantID'))));
@@ -33,16 +22,10 @@ class Placement extends \Magento\Framework\App\Action\Action
         $checkoutSession->setTransactiontype(base64_decode($this->getRequest()->getParam('TransactionType')));
         $checkoutSession->setTransactiondatetime(base64_decode($this->getRequest()->getParam('TransactionDateTime')));
         $checkoutSession->setOrderdescription(base64_decode($this->getRequest()->getParam('OrderDescription')));
-        $checkoutSession->setCustomername(base64_decode($this->getRequest()->getParam('CustomerName')));
-        $checkoutSession->setAddress1(base64_decode($this->getRequest()->getParam('Address1')));
-        $checkoutSession->setAddress2(base64_decode($this->getRequest()->getParam('Address2')));
-        $checkoutSession->setAddress3(base64_decode($this->getRequest()->getParam('Address3')));
-        $checkoutSession->setAddress4(base64_decode($this->getRequest()->getParam('Address4')));
         $checkoutSession->setCity(base64_decode($this->getRequest()->getParam('City')));
         $checkoutSession->setState(base64_decode($this->getRequest()->getParam('State')));
         $checkoutSession->setPostcode(base64_decode($this->getRequest()->getParam('PostCode')));
         $checkoutSession->setLastRealOrderId(base64_decode($this->getRequest()->getParam('LastRealOrderId')));
-        $checkoutSession->setPayFortCwTransactionId(base64_decode($this->getRequest()->getParam('Payforttransactionid')));
 
         $this->_redirect('redirect/url/here');
     }
