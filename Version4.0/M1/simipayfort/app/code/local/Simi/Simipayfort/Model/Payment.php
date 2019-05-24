eee9b813cdc0f249d10226b0d0ffa5f16ad13ef3
Cody Nguyen <cody@simicart.com>
2019-05-24 10:51:30 +0700

    Payfort from https://github.com/payfort/magento-payfort

--- Version4.0/M1/simipayfort/app/code/local/Simi/Simipayfort/Model/Payment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Simi_Simipayfort_Model_Payment extends Mage_Payment_Model_Method_Abstract 
-{
-
-    protected $_code = 'simipayfort';    
-    protected $_infoBlockType = 'simipayfort/payment';
-}
