34fa0ba9d7ede9093ff91cc49b18c53c5a46e934
namhoang2201 <hoangnamtb95@gmail.com>
2019-08-07 11:28:45 +0700

    Fix bug tab Store's Holiday not loaded when create new or save a store.

--- Version4.0/M2/storelocator/app/code/Simi/Simistorelocator/Block/Adminhtml/Holiday/Edit/Tabs.php
+++ Version4.0/M2/storelocator/app/code/Simi/Simistorelocator/Block/Adminhtml/Holiday/Edit/Tabs.php
@@ -11,7 +11,7 @@ class Tabs extends \Magento\Backend\Block\Widget\Tabs {
      */
     protected function _construct() {
         parent::_construct();
-        $this->setId('general_tabs');
+        $this->setId('holiday_tabs');
         $this->setDestElementId('edit_form');
         $this->setTitle(__('Holiday Information'));
     }
