88e9289054a81e8232c9340493de59ae5d636996
Cody Nguyen <cody@simicart.com>
2020-04-16 16:52:14 +0700

    siminia ppexpress

---
+++ Version4.0/M2/siminiapaypalexpress/app/code/Simi/Siminiapaypalexpress/Observer/Frontendcontrollerpredispatch.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Simi\Siminiapaypalexpress\Observer;
+
+use Magento\Framework\Event\Observer;
+use \Magento\Framework\Event\ObserverInterface;
+use \Magento\Framework\ObjectManagerInterface as ObjectManager;
+
+class Frontendcontrollerpredispatch implements ObserverInterface
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface as ObjectManager
+     */
+    private $simiObjectManager;
+
+    public function __construct(ObjectManager $simiObjectManager)
+    {
+        $this->simiObjectManager = $simiObjectManager;
+    }
+    public function execute(Observer $observer)
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $pos = strpos($uri,'paypal_express.html');
+        if ($pos === false)
+            $pos = strpos($uri,'paypal_express_falure.html');
+        $link = $this->simiObjectManager->get('Magento\Framework\App\Config\ScopeConfigInterface')
+            ->getValue('simiconnector/general/pwa_studio_url');
+        if($pos && $link){
+            $newUri = $link . substr($uri, $pos);
+            header("Location: {$newUri}");
+            exit;
+        }
+    }
+
+}
